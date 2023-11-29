b285caa3c4fbd0f1e7b1aa45e0c1f58e625d87ca
ensea <ensea@StudentLab>
2023-11-29 14:45:52 +0100

    Question 1

---
+++ TP/enseash.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+	char buf[] = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+	size_t size1 = strlen(buf);
+	write(STDOUT_FILENO,buf,size1);
+}
